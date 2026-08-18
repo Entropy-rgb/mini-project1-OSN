29f5bdb9a5589521eb051bb68d101675a136fc40
Entropy-rgb <som3shkamad@gmail.com>
2026-08-18 16:53:23 +0530

    add input loop , with printing of username, hostname, and the relative working directory (#1)

--- c-shell/src/main.c
+++ c-shell/src/main.c
@@ -6,28 +6,32 @@
 #include <sys/types.h>
 #include <limits.h>
 
-
-int main(){
-    //get username using the getpwuid syscall
+int main()
+{
+    // get username using the getpwuid syscall
     uid_t user_uid = getuid();
-    struct passwd* pw = getpwuid(user_uid);
+    struct passwd *pw = getpwuid(user_uid);
 
-    //get hostname using gethostname()
+    // get hostname using gethostname()
     char hostname[_SC_HOST_NAME_MAX];
     gethostname(hostname, _SC_HOST_NAME_MAX);
 
-    //get the shell cwd , which is the shell home according to the requirement doc
+    // get the shell cwd , which is the shell home according to the requirement doc
     char shell_home[4096];
     getcwd(shell_home, sizeof(shell_home));
     int home_len = strlen(shell_home);
 
-    while(true){
+    while (true)
+    {
         char cwd[4096];
         getcwd(cwd, sizeof(cwd));
         char rel_working_dir[4096] = "~";
-        if(strncmp(cwd, shell_home, home_len) == 0 && (cwd[home_len] == '\0' || cwd[home_len] == '\\')){
+        if (strncmp(cwd, shell_home, home_len) == 0 && (cwd[home_len] == '\0' || cwd[home_len] == '\\'))
+        {
             strcat(rel_working_dir, &cwd[home_len]);
-        }else{
+        }
+        else
+        {
             strcpy(rel_working_dir, cwd);
         }
         printf("<%s@%s:%s>", pw->pw_name, hostname, rel_working_dir);
