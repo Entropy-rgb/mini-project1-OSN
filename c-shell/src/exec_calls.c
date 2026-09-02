2ddba08b4030ec5a5439b6f4b27114b099e91027
Entropy-rgb <som3shkamad@gmail.com>
2026-09-02 15:37:48 +0530

    refactor: extract single-command executor and report not-found status

--- c-shell/src/exec_calls.c
+++ c-shell/src/exec_calls.c
@@ -4,117 +4,118 @@
 #include <string.h>
 #include <sys/wait.h>
 
-void execute_external(char **args, int arg_count)
+static int check_access_exists(char **args, int arg_count)
 {
-    pid_t pid = fork();
-
-    if (pid < 0)
-    {
-        perror("fork");
-        return;
-    }
-
-    if (pid > 0)
-    {
-        wait(NULL);
-        return;
-    }
-
-    if (strchr(args[0], '/') != NULL)
-    {
+    if (strchr(args[0], '/') != NULL) {
         if (access(args[0], X_OK) == 0)
-        {
-            execv(args[0], args);
-            perror("execv");
-            exit(1);
-        }
-
-        fprintf(stderr, "cshell: command not found (%s)\n", args[0]);
-        exit(1);
+            return 1;
+        return 0;
     }
+    char *cmd = args[0];
+    char *lookup = cmd;
+    if (cmd[0] == '%')
+        lookup = cmd + 1;
+    if (lookup[0] == '\0')
+        return 0;
+    char local_path[4096];
+    snprintf(local_path, sizeof(local_path), "./%s", lookup);
+    if (access(local_path, X_OK) == 0)
+        return 1;
+    char *path = getenv("PATH");
+    if (path == NULL)
+        return 0;
+    char *path_copy = strdup(path);
+    if (path_copy == NULL)
+        return 0;
+    int found = 0;
+    char *dir = strtok(path_copy, ":");
+    while (dir != NULL) {
+        char found_path[4096];
+        snprintf(found_path, sizeof(found_path), "%s/%s", dir, lookup);
+        if (access(found_path, X_OK) == 0) {
+            found = 1;
+            break;
+        }
+        dir = strtok(NULL, ":");
+    }
+    free(path_copy);
+    return found;
+}
 
+int execute_external(char **args, int arg_count)
+{
+    if (arg_count == 0 || args[0] == NULL)
+        return 0;
+    if (check_access_exists(args, arg_count) == 0) {
+        char *name = args[0];
+        if (name[0] == '%')
+            name = name + 1;
+        fprintf(stderr, "cshell: command not found (%s)\n", name);
+        return 1;
+    }
+    pid_t pid = fork();
+    if (pid < 0) {
+        perror("fork");
+        return 0;
+    }
+    if (pid > 0) {
+        int status;
+        waitpid(pid, &status, 0);
+        return 0;
+    }
+    if (strchr(args[0], '/') != NULL) {
+        execv(args[0], args);
+        perror("execv");
+        _exit(1);
+    }
     char *cmd_name = args[0];
-
-    if (args[0][0] == '%')
-    {
+    if (args[0][0] == '%') {
         cmd_name = args[0] + 1;
         args[0] = cmd_name;
-
         char *path = getenv("PATH");
-        if (path != NULL)
-        {
+        if (path != NULL) {
             char *path_copy = strdup(path);
-            if (path_copy != NULL)
-            {
+            if (path_copy != NULL) {
                 char *dir = strtok(path_copy, ":");
-
-                while (dir != NULL)
-                {
+                while (dir != NULL) {
                     char found_path[4096];
-
-                    snprintf(found_path, sizeof(found_path),
-                             "%s/%s", dir, cmd_name);
-
-                    if (access(found_path, X_OK) == 0)
-                    {
+                    snprintf(found_path, sizeof(found_path), "%s/%s", dir, cmd_name);
+                    if (access(found_path, X_OK) == 0) {
                         execv(found_path, args);
                         perror("execv");
-                        exit(1);
+                        _exit(1);
                     }
-
                     dir = strtok(NULL, ":");
                 }
-
                 free(path_copy);
             }
         }
-
-        fprintf(stderr, "cshell: command not found (%s)\n", cmd_name);
-        exit(1);
+        _exit(1);
     }
-
     char local_path[4096];
-
     snprintf(local_path, sizeof(local_path), "./%s", args[0]);
-
-    if (access(local_path, X_OK) == 0)
-    {
+    if (access(local_path, X_OK) == 0) {
         execv(local_path, args);
         perror("execv");
-        exit(1);
+        _exit(1);
     }
-
     char *path = getenv("PATH");
-
-    if (path != NULL)
-    {
+    if (path != NULL) {
         char *path_copy = strdup(path);
-
-        if (path_copy != NULL)
-        {
+        if (path_copy != NULL) {
             char *dir = strtok(path_copy, ":");
-
-            while (dir != NULL)
-            {
+            while (dir != NULL) {
                 char found_path[4096];
-
-                snprintf(found_path, sizeof(found_path),
-                         "%s/%s", dir, args[0]);
-
-                if (access(found_path, X_OK) == 0)
-                {
+                snprintf(found_path, sizeof(found_path), "%s/%s", dir, args[0]);
+                if (access(found_path, X_OK) == 0) {
                     execv(found_path, args);
                     perror("execv");
-                    exit(1);
+                    _exit(1);
                 }
-
                 dir = strtok(NULL, ":");
             }
-
             free(path_copy);
         }
     }
-
-    fprintf(stderr, "cshell: command not found (%s)\n", args[0]);
-    exit(1);
+    _exit(1);
 }
--- c-shell/src/main.c
+++ c-shell/src/main.c
@@ -20,187 +20,146 @@
 #define HOST_NAME_MAX 256
 #endif
 
+int execute_single(char **args, int arg_count, char *shell_home, char *prev_dir)
+{
+    int saved_stdin = dup(STDIN_FILENO);
+    int saved_stdout = dup(STDOUT_FILENO);
+    if (saved_stdin < 0 || saved_stdout < 0) {
+        perror("dup");
+        if (saved_stdin >= 0)
+            close(saved_stdin);
+        if (saved_stdout >= 0)
+            close(saved_stdout);
+        return 0;
+    }
+    char **clean_args = malloc(sizeof(char *) * (arg_count + 1));
+    if (clean_args == NULL) {
+        close(saved_stdin);
+        close(saved_stdout);
+        return 0;
+    }
+    int clean_count = 0;
+    for (int i = 0; i < arg_count; i++) {
+        if (strcmp(args[i], "<") == 0 || strcmp(args[i], ">") == 0 || strcmp(args[i], ">>") == 0) {
+            i++;
+            continue;
+        }
+        clean_args[clean_count++] = args[i];
+    }
+    clean_args[clean_count] = NULL;
+    if (clean_count == 0) {
+        close(saved_stdin);
+        close(saved_stdout);
+        free(clean_args);
+        return 0;
+    }
+    int input_status = setup_input_redirection(args, arg_count);
+    if (input_status == -1) {
+        dup2(saved_stdin, STDIN_FILENO);
+        dup2(saved_stdout, STDOUT_FILENO);
+        close(saved_stdin);
+        close(saved_stdout);
+        free(clean_args);
+        return 0;
+    }
+    FILE *output_tmp = NULL;
+    int output_status = setup_output_redirection(args, arg_count, &output_tmp);
+    if (output_status == -1) {
+        dup2(saved_stdin, STDIN_FILENO);
+        dup2(saved_stdout, STDOUT_FILENO);
+        close(saved_stdin);
+        close(saved_stdout);
+        free(clean_args);
+        return 0;
+    }
+    int ret = 0;
+    if (strcmp(clean_args[0], "hop") == 0) {
+        execute_hop(clean_args, clean_count, shell_home, prev_dir);
+    } else if (strcmp(clean_args[0], "reveal") == 0) {
+        execute_reveal(clean_args, clean_count, shell_home, prev_dir);
+    } else if (strcmp(clean_args[0], "peek") == 0) {
+        peek(clean_count, clean_args);
+    } else if (strcmp(clean_args[0], "locate") == 0) {
+        locate(clean_count, clean_args);
+    } else {
+        ret = execute_external(clean_args, clean_count);
+    }
+    if (dup2(saved_stdin, STDIN_FILENO) < 0) {
+        perror("dup2");
+    }
+    if (dup2(saved_stdout, STDOUT_FILENO) < 0) {
+        perror("dup2");
+    }
+    close(saved_stdin);
+    close(saved_stdout);
+    if (output_status == 1) {
+        distribute_output(args, arg_count, output_tmp);
+    }
+    free(clean_args);
+    return ret;
+}
+
 int main()
 {
-    // get username using the getpwuid syscall
     uid_t user_uid = getuid();
     struct passwd *pw = getpwuid(user_uid);
-
     const char *username = "unknown";
-    if (pw == NULL)
-    {
+    if (pw == NULL) {
         fprintf(stderr, "shell : could not resolve username\n");
-    }
-    else
-    {
+    } else {
         username = pw->pw_name;
     }
-
-    // get hostname using gethostname()
     char hostname[HOST_NAME_MAX];
-    if (gethostname(hostname, HOST_NAME_MAX) != 0)
-    {
+    if (gethostname(hostname, HOST_NAME_MAX) != 0) {
         strcpy(hostname, "unknown");
     }
-
-    // get the shell cwd , which is the shell home according to the requirement doc
     char shell_home[4096];
     getcwd(shell_home, sizeof(shell_home));
-
     char *line = NULL;
     size_t len = 0;
     char prev_dir[4096] = "";
-    while (true)
-    {
+    while (true) {
         display_prompt(username, hostname, shell_home);
         ssize_t nread = getline(&line, &len, stdin);
-        if (nread == -1)
-        {
+        if (nread == -1) {
             break;
         }
         line[strcspn(line, "\n")] = '\0';
-        if (strchr(line, '|') != NULL)
-        {
+        if (strchr(line, '|') != NULL) {
             execute_pipeline(line, shell_home, prev_dir);
             continue;
         }
-        // send the input to lexer, get the lexer's output and send it to parser , and execute given commands
         int ok = 1;
         token *token_head = lexer(line, &ok);
-
-        if (ok == 0 || token_head == NULL)
-        {
+        if (ok == 0 || token_head == NULL) {
             if (token_head)
                 free_tokens(token_head);
             continue;
         }
-
         int grammar_check = parser(token_head);
-        if (grammar_check == 1)
-        {
+        if (grammar_check == 1) {
             fprintf(stderr, "cshell: invalid syntax\n");
-            free_tokens(token_head); // Clean up memory before continuing
+            free_tokens(token_head);
             continue;
         }
-
-        // making the array of strings , so that they will also be useful in future subparts of the assignment
         token *tmp = token_head;
         int arg_count = 0;
-        while (tmp != NULL)
-        {
+        while (tmp != NULL) {
             arg_count++;
             tmp = tmp->next_token;
         }
-
         char **args = malloc(sizeof(char *) * (arg_count + 1));
-        if (args == NULL)
-        {
+        if (args == NULL) {
             free_tokens(token_head);
             continue;
         }
         tmp = token_head;
-        for (int i = 0; i < arg_count; i++)
-        {
+        for (int i = 0; i < arg_count; i++) {
             args[i] = tmp->content;
             tmp = tmp->next_token;
         }
         args[arg_count] = NULL;
-
-        int saved_stdin = dup(STDIN_FILENO);
-        int saved_stdout = dup(STDOUT_FILENO);
-        if (saved_stdin < 0 || saved_stdout < 0)
-        {
-            perror("dup");
-            if (saved_stdin >= 0)
-                close(saved_stdin);
-            if (saved_stdout >= 0)
-                close(saved_stdout);
-            free_tokens(token_head);
-            free(args);
-            continue;
-        }
-        char **clean_args =
-            malloc(sizeof(char *) * (arg_count + 1));
-        if (clean_args == NULL)
-        {
-            close(saved_stdin);
-            close(saved_stdout);
-            free_tokens(token_head);
-            free(args);
-            continue;
-        }
-        int clean_count = 0;
-        for (int i = 0; i < arg_count; i++)
-        {
-            if (strcmp(args[i], "<") == 0 || strcmp(args[i], ">") == 0 || strcmp(args[i], ">>") == 0)
-            {
-                i++;
-                continue;
-            }
-            clean_args[clean_count++] = args[i];
-        }
-        clean_args[clean_count] = NULL;
-        int input_status = setup_input_redirection(args, arg_count);
-        if (input_status == -1)
-        {
-            dup2(saved_stdin, STDIN_FILENO);
-            dup2(saved_stdout, STDOUT_FILENO);
-            close(saved_stdin);
-            close(saved_stdout);
-            free(clean_args);
-            free_tokens(token_head);
-            free(args);
-            continue;
-        }
-        FILE *output_tmp = NULL;
-        int output_status =
-            setup_output_redirection(args, arg_count, &output_tmp);
-        if (output_status == -1)
-        {
-            dup2(saved_stdin, STDIN_FILENO);
-            dup2(saved_stdout, STDOUT_FILENO);
-            close(saved_stdin);
-            close(saved_stdout);
-            free(clean_args);
-            free_tokens(token_head);
-            free(args);
-            continue;
-        }
-        if (strcmp(clean_args[0], "hop") == 0)
-        {
-            execute_hop(clean_args, clean_count, shell_home, prev_dir);
-        }
-        else if (strcmp(clean_args[0], "reveal") == 0)
-        {
-            execute_reveal(clean_args, clean_count, shell_home, prev_dir);
-        }
-        else if (strcmp(clean_args[0], "peek") == 0)
-        {
-            peek(clean_count, clean_args);
-        }
-        else if (strcmp(clean_args[0], "locate") == 0)
-        {
-            locate(clean_count, clean_args);
-        }
-        else
-        {
-            execute_external(clean_args, clean_count);
-        }
-        if (dup2(saved_stdin, STDIN_FILENO) < 0)
-        {
-            perror("dup2");
-        }   
-        if (dup2(saved_stdout, STDOUT_FILENO) < 0)
-        {
-            perror("dup2");
-        }
-        close(saved_stdin);
-        close(saved_stdout);
-        if (output_status == 1)
-        {
-            distribute_output(args, arg_count, output_tmp);
-        }
-        free(clean_args);
+        execute_single(args, arg_count, shell_home, prev_dir);
         free_tokens(token_head);
         free(args);
     }
