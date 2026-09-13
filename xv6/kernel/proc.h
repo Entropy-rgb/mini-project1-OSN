520c0bacf46a2206764658f03338354aca861a0a
Entropy-rgb <som3shkamad@gmail.com>
2026-09-13 19:56:52 +0530

    feat(proc): add scheduler properties and time metrics to struct proc

--- xv6/kernel/proc.h
+++ xv6/kernel/proc.h
@@ -88,6 +88,12 @@ struct proc {
   int killed;           // If non-zero, have been killed
   int xstate;           // Exit status to be returned to parent's wait
   int pid;              // Process ID
+  uint64 creation_time;
+  uint64 ctime;
+  uint64 rtime;
+  uint64 etime;
+  uint64 iotime;
+  uint64 first_run_time;
 
   // wait_lock must be held when using this:
   struct proc *parent; // Parent process
